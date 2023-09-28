2d64ccc96d1bb81a18358396dca6674865873bc2
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2023-09-28 15:42:23 -0300

    fix: billboard/painel

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,19 +8,20 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-
-  const billboard = await getBillboard("d0204d81-ddad-40bf-97cd-912533b83955");
+  const billboard = await getBillboard("fdc5efa9-92c6-4931-8222-ba2997a5f80a");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
+        <Billboard 
+          data={billboard}
+        />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Produtos em Destaque" items={products} />
+          <ProductList title="Destaques" items={products} />
         </div>
       </div>
     </Container>
-  );
+  )
 };
 
 export default HomePage;
