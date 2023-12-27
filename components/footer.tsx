b61c90514e2560178e94767139123bf6cd98fe6c
Paulo Santos <paulo_santos20@outlook.com.br>
2023-12-27 11:41:47 -0300

    style:Mary Maria Moda Fitness

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,15 @@ const Footer = () => {
     return (
       <footer className="bg-white border-t">
         <div className="mx-auto py-10">
+        NORDIK FITWEAR
+
+CNPJ: 42.365.901/0001-27
+CPF: 104.933.804-93
+Telefone: (81) 9696-4449
+Boa Viagem, Recife - PE, 51021-350
+Todos os direitos reservados
           <p className="text-center text-xs text-black">
-            &copy; 2023 Veneza Shopping Store, Inc. All rights reserved.
+            &copy; 2023 Mary Maria Moda Fitness, Inc. All rights reserved.
           </p>
         </div>
       </footer>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from 'next/image';
 
 import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
@@ -10,12 +11,15 @@ export const revalidate = 0;
 const Navbar = async () => {
   const categories = await getCategories();
 
-  return ( 
+  return (
     <div className="bg-pink-300">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex flex-col h-30 items-center">
           <Link href="/" className="bg-white p-5 ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">Veneza Shopping</p>
+            <p className="font-bold text-xl">Mary Maria Moda Fitness</p>
+            <div className="image-section">
+                <Image src="/mary.jpg" alt="Mary Maria Moda Fitness" width={100} height={50} />
+              </div>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
@@ -24,5 +28,5 @@ const Navbar = async () => {
     </div>
   );
 };
- 
+
 export default Navbar;
