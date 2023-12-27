d5c6d557b071485270aa5fda5cd5710ac75e4c6f
Paulo Santos <paulo_santos20@outlook.com.br>
2023-12-27 11:55:00 -0300

    style: navbar

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,9 +4,17 @@ const Footer = () => {
       <div className="mx-auto py-10 flex flex-col items-center">
         <div className="text-center mb-4">
           CNPJ: 42.365.901/0001-27
+        </div>
+        <div className="text-center mb-4">
           CPF: 104.933.804-93
+        </div>
+        <div className="text-center mb-4">
           Telefone: (81) 9696-4449
+        </div>
+        <div className="text-center mb-4">
           Boa Viagem, Recife - PE, 51021-350
+        </div>
+        <div className="text-center mb-4">
           Todos os direitos reservados
         </div>
         <p className="text-center text-xs text-black">
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,8 +16,7 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex flex-col h-30 items-center">
           <Link href="/" className="bg-white p-5 ml-4 flex lg:ml-0 gap-x-2">
-            <a className="bg-white p-5 ml-4 flex lg:ml-0 gap-x-2">
-              <p className="font-bold text-xl text-center">Mary Maria Moda Fitness</p>
+            <a className="bg-white p-5 ml-4 flex lg:ml-0 gap-x-2">              
               <div className="image-section">
                 <Image src="/mary.jpg" alt="Mary Maria Moda Fitness" width={100} height={50} />
               </div>
